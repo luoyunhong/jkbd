855bc08b80f59749fcf31918d4894a878d6085f5
luoyunhong <1438130802@qq.com>
2017-06-29 17:01:30 +0800

    布局

---
+++ app/src/main/java/com/example/administrator/jkbd/activity/QuestionActivity.java
@@ -0,0 +1,19 @@
+package com.example.administrator.jkbd.activity;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.administrator.jkbd.R;
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class QuestionActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
