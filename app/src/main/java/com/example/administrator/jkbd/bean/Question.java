ef83cc9e264727c03a906ac55ab8b738b17e607e
luoyunhong <1438130802@qq.com>
2017-06-29 13:03:26 +0800

    修改Question实体类

--- app/src/main/java/com/example/administrator/jkbd/bean/Question.java
+++ app/src/main/java/com/example/administrator/jkbd/bean/Question.java
@@ -28,6 +28,8 @@ public class Question {
     private String explains;
     private String url;
 
+    private String userAnswer;
+
     public int getId() {
         return id;
     }
@@ -98,5 +100,13 @@ public class Question {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getUserAnswer() {
+        return userAnswer;
+    }
+
+    public void setUserAnswer(String userAnswer) {
+        this.userAnswer = userAnswer;
     }
 }
