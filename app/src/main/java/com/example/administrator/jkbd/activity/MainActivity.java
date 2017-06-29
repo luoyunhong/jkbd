1b4b3a4184c9f2923bad2595a004616b139dfe8c
luoyunhong <1438130802@qq.com>
2017-06-29 16:13:28 +0800

    将activity发到对应的包里

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.administrator.jkbd;
+package com.example.administrator.jkbd.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.example.administrator.jkbd.R;
 
 public class MainActivity extends AppCompatActivity {
 
