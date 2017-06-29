9a93a445798c88688cf1ee3108b10660af08441e
luoyunhong <1438130802@qq.com>
2017-06-29 23:40:26 +0800

    从网络上下载数据，显示

--- app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.administrator.jkbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.administrator.jkbd.R;
+import com.example.administrator.jkbd.bean.ExamInfo;
+import com.example.administrator.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void test(View view) {
+        OkHttpUtils<ExamInfo> utils=new OkHttpUtils<>(getApplicationContext());
+        String url="http://101.251.196.90:8080/JztkServer/examInfo";
+        utils.url(url)
+                .targetClass(ExamInfo.class)
+                .execute(new OkHttpUtils.OnCompleteListener<ExamInfo>() {
+                    @Override
+                    public void onSuccess(ExamInfo result) {
+                        Log.e("main","result="+result);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        Log.e("main","error="+error);
+                    }
+                });
         startActivity(new Intent(MainActivity.this,QuestionActivity.class));
     }
 
